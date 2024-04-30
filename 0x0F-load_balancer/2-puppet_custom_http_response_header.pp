c5fb3c1ddaa5e8272f58300ffb9973431db73e8e
Nomcebo1992 <snembambo@gmail.com>
2024-04-29 17:02:08 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,16 @@
-# Install Nginx on server
-package { 'nginx':
-  ensure => installed,
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-
-file_line { 'add custom header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "\tadd_header X-Served-By ${hostname};",
-  after  => 'server_name _;',
+-> package {'nginx':
+  ensure => 'present',
 }
-
-
-service { 'nginx':
-  ensure => running,
-  require => Package[ 'nginx' ]
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
